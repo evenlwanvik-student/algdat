f04e7fbab2428ef9022ad13687d0b7bbe4681a66
evenlwanvik-student <42798352+evenlwanvik-student@users.noreply.github.com>
2018-08-29 12:03:50 +0200

    Create assignment-01.jl
    assignment 01 100% score

---
+++ assignment-01.jl
@@ -0,0 +1,14 @@
+function inser-sort!(A)
+    if length(A) < 1 # check if A is empty
+        return "A < 1"
+    end
+    for i in 2:length(A) # Julia uses 1 based indexing, no point at starting at 1 wchich has 1 element
+        key = A[i] # Set key equal to the element we're currently checking
+        j = i-1 # j will be the index checking the elements before i
+        while j > 0 && key < A[j] # loop until value is less than the current element
+            A[j+1] = A[j]
+            j = j - 1
+        end
+        A[j+1] = key # set equal to the last index that was bigger than current
+    end
+end
